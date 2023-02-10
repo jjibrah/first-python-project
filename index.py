9a1d51a8d89806614312b99b92c83eb0234faef1
jjibrah <ibrabdi109@gmail.com>
2023-02-10 09:02:41 +0300

    first edit

--- index.py
+++ index.py
@@ -1,25 +1,31 @@
-from main import Fruit
+from main import Fruit, Car, Employee , Student
 
 # Start creating fruits out of the fruit class
+
 fruit_one = Fruit("Mango", "200g", "Green", "Ksh. 50.00")
 fruit_two = Fruit("Apple", "150g", "pink", "Ksh. 30.00")
 
 print(fruit_two.name)
 
-from main import Car, Employee
+# Start creating cars out of the car class
 
 cars1 = Car('Audi', 'Matte Green', '300Hp', 'KSH 2,300,000')
 cars2 = Car('FORD', 'chrome', '600Hp', 'KSH 4,100,000')
 cars3 = Car('Mercedes', 'Matte Black', '340Hp', 'KSH 23,000,000')
 
 print(cars1.horse_power)
+# Start creating employees out of the employee class
 
 employee1 = Employee('Bill Claxton', 'C.E.O', '14 Years', '800,000')
 employee2 = Employee('Stephen Poole', 'Assistant Director', '10 Years', '700,000')
 employee3 = Employee('Tommy Hardaway', 'IT Mnager', '6 Years', '620,000')
 employee4 = Employee('Fred Vanvleet', 'Software Developer', '2 Years', '400,000')
 
-print(employee1.name, employee2.name, employee3.name, employee4.name)
-print(employee1.post, employee2.post, employee3.post, employee4.post)
-print(employee1.experience, employee2.experience, employee3.experience, employee4.experience)
-print(employee1.salary, employee2.salary, employee3.salary, employee4.salary, )
+print(employee1.name)
+
+
+# Start creating students out of the student class
+student_one = Student("Josiah","josiah@gmail.com","josia123")
+student_two = Student("James","james@gmail.com","james123")
+
+print(student_one.email)
--- main.py
+++ main.py
@@ -16,12 +16,25 @@ class Car:
 
 
 
-
-
-
 class Employee:
     def __init__(self, name, post, experience, salary):
         self.name = name
         self.post = post
         self.experience = experience
         self.salary = salary
+
+class Student:
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+    def register(self):
+        print("you registered with email",self.email,"and password",self.password)
+
+    def login(self):
+        #assume the user has already registered
+        if self.email == "abc@example.com" and self.password == "123":
+            print("login successful")
+        else:
+            print("Please enter the correct username or password")
